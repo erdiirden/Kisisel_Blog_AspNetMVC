d6a84642f9674fe2dcd9e5141b922cde6209a5fa
Erdi İrden <113932351+erdiirden@users.noreply.github.com>
2025-03-21 04:40:47 +0300

    Generic Repository

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -8,9 +8,9 @@ namespace DataAccessLayer.Abstract
 {
     public interface IGenericDal<T> where T:class
     {
-        void Insert (T item);  
-        void Delete (T item);
-        void Update (T item);
+        void Insert (T t);  
+        void Delete (T t);
+        void Update (T t);
         List<T> GetList ();
         T GetByID (int id);
     }
