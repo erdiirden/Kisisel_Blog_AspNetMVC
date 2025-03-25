ef26236cc6b8513a48d7d83a12b3808ad0cc7a29
Erdi İrden <113932351+erdiirden@users.noreply.github.com>
2025-03-26 00:19:00 +0300

    Manager Class

---
+++ BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        ITestimonailDal _testimonialDal;
+
+        public TestimonialManager(ITestimonailDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+
+        public void TAdd(Testimonail t)
+        {
+            _testimonialDal.Insert(t);
+        }
+
+        public void TDelete(Testimonail t)
+        {
+            _testimonialDal.Delete(t);
+        }
+
+        public Testimonail TGetByID(int id)
+        {
+            return _testimonialDal.GetByID(id);
+        }
+
+        public List<Testimonail> TGetList()
+        {
+            return _testimonialDal.GetList();
+        }
+
+        public void TUpdate(Testimonail t)
+        {
+            _testimonialDal.Update(t);
+        }
+    }
+}
